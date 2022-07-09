6c5cc84d264da569338748880eae7e665b5f7678
thathardwareguy <ardutech11@gmail.com>
2022-07-09 19:09:42 +0100

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=olamidealade11
+export POSTGRES_HOST=udacity-microservices-db.ccwrwolivt6r.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udacity-microservices-s3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
